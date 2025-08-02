e91db99242ad4818faffd8bfc939169b0e597d4e
ryoyamasuda <ryouya_masuda@unirita.co.jp>
2025-08-03 01:16:10 +0900

    comit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ProductComponent } from './product/product.component';
+import { ProductModule } from './product/product.module';
+import { ProductDetailComponent } from './product/product-detail/product-detail.component';
+import { ProductListComponent } from './product/product-listings/product-listings.component';
 
 @NgModule({
   declarations: [
