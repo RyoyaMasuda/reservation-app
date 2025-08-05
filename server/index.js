4944f238cc84d99fa1d0ffcd9022bc4e538ee021
ryoyamasuda <ryouya_masuda@unirita.co.jp>
2025-08-05 16:42:43 +0900

    commit

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const mongoose = require('mongoose')
-
+const config = require('./config/dev')
+ 
+// .net/Cluster0?retryWrites= のようにCluster0(データベース名)をいれないといけないっぽい
+mongoose.connect(config.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 const app = express()
 
@@ -13,46 +19,3 @@ const PORT = process.env.PORT || '3001'
 app.listen('3001', function() {
     console.log('I am running!')
 })
-
-
-// あなたのMongoDB接続URI
-// <password> は必ず実際のパスワードに置き換えてください
-const uri = "mongodb+srv://despocampifoot:testtest@cluster0.yccebok.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-// Mongoose または MongoClient のインスタンスを作成
-// Mongoose v5.x では useNewUrlParser と useUnifiedTopology が必要です
-const client = new mongoose.Mongoose(); // Mongooseを使う場合
-// または const client = new MongoClient(uri, { ... }); // mongodbドライバーを使う場合
-
-async function connectToDatabase() {
-  try {
-    // mongoose.connect の場合
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-      // Mongoose v5.xでは serverApi オプションは不要です
-    });
-    console.log("MongoDBに正常に接続しました！");
-
-    // mongodbドライバーの MongoClient を使っている場合 (connet2mongoDB.js の方)
-    // await client.connect();
-    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
-
-  } catch (err) {
-    console.error("MongoDB接続エラー:", err);
-    process.exit(1); // 接続に失敗した場合、プロセスを終了します
-  }
-}
-
-// async関数を呼び出して接続を開始します
-connectToDatabase();
-
-// ここに他のExpress.jsやサーバーのコードが続きます
-// 例：データベース接続後にサーバーを起動する場合
-// connectToDatabase().then(() => {
-//   // app.listen(port, () => {
-//   //   console.log(`Server running on port ${port}`);
-//   // });
-// }).catch(err => {
-//   console.error("DBエラーのためサーバーの起動に失敗しました:", err);
-// });
